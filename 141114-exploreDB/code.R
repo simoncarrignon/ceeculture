c3085c3087a1b62a5d7f9d206042524db3d33b50
Jean-Marc <montanier.jeanmarc@gmail.com>
2014-11-20 14:32:45 +0100

    some request and results on the database. preliminary look around the db

--- 141114-exploreDB/code.R
+++ 141114-exploreDB/code.R
@@ -1,22 +1,26 @@
+#!/usr/bin/Rscript
+#yo
 
 library(ggplot2)
 library(gridExtra)
 
-data<- read.csv('la_catria_dataset_space.txt', sep='\t', header=TRUE)
 
 # frequency of stamps and places
 
-tmpTable<- data.frame(lst=data$Stamp_SimpleText)
-stampFreq <- as.data.frame(table(tmpTable$lst))
+dataAllStamp<- read.csv('allStamps.csv', sep=';', header=TRUE)
+tmpTable<- data.frame(lst=dataAllStamp$SimpleText)
+allStampFreq <- as.data.frame(table(tmpTable$lst))
 
-tmpTable<- data.frame(lst=data$FindingPlace)
-locFreq <- as.data.frame(table(tmpTable$lst))
+dataStampTit<- read.csv('stampsWithTit.csv', sep=';', header=TRUE)
+tmpTable<- data.frame(lst=dataStampTit$SimpleText)
+stampTitFreq <- as.data.frame(table(tmpTable$lst))
+
 
 # temporal distribution of values    
 
-tmpData<- read.csv('la_catria_dataset_time.txt', sep='\t', header=TRUE)
-minYear = min(tmpData$Dating_StartYear)
-maxYear = max(tmpData$Dating_EndYear)
+dataTemporal<- read.csv('dating.tmp.csv', sep=';', header=TRUE)
+minYear = min(dataTemporal$StartYear)
+maxYear = max(dataTemporal$EndYear)
 
 time <- data.frame(year=seq(minYear, maxYear))
 time$volume = 0
@@ -24,7 +28,7 @@ time$volume = 0
 
 countRepetitions <- function(tmpDataRow, year)
 {
-    if(year>=tmpDataRow['Dating_StartYear'] & year<=tmpDataRow['Dating_EndYear'])
+    if(year>=tmpDataRow['StartYear'] & year<=tmpDataRow['EndYear'])
     {
         return(1)
     }
@@ -33,18 +37,18 @@ countRepetitions <- function(tmpDataRow, year)
 
 computeVolume <- function(row)
 {
-    row['volume'] = sum(apply(tmpData, 1, countRepetitions, row['year']))
+    row['volume'] = sum(apply(dataTemporal, 1, countRepetitions, row['year']))
 }
 
 time$volume = apply(time, 1, computeVolume)
 
 
 
-g1 <- ggplot(stampFreq, aes(x=Freq)) + geom_histogram(binwidth=1) + ggtitle('frequency distribution of stamps') + scale_x_sqrt(breaks=c(1,5,10, seq(10,50,10),seq(50,max(stampFreq$Freq),50))) + xlab('Freq (sqrt scale)') + ylab('stamp instances')
-g2 <- ggplot(locFreq, aes(x=Freq)) + geom_histogram(binwidth=1) + ggtitle('frequency distribution of finding locations') + scale_x_sqrt(breaks=c(0,1,5,10, seq(10,50,10),seq(50,max(locFreq$Freq),50))) + xlab('Freq (sqrt scale)') + ylab('stamps per location')
-g3 <- ggplot(time, aes(x=year, y=volume)) + geom_line() + ggtitle('temporal distribution of stamp findings')
+g1 <- ggplot(allStampFreq, aes(x=Freq)) + geom_histogram(binwidth=1) + ggtitle('frequency distribution of all stamps') + scale_x_sqrt(breaks=c(1,5,10, seq(10,50,10),seq(50,max(allStampFreq$Freq),50))) + xlab('Freq (sqrt scale)') + ylab('stamp instances')
+g2 <- ggplot(stampTitFreq, aes(x=Freq)) + geom_histogram(binwidth=1) + ggtitle('frequency distribution stamps with titulus pictus') + xlab('Freq') + ylab('stamp instances')
+g3 <- ggplot(time, aes(x=year, y=volume)) + geom_line() + ggtitle('temporal distribution of titulus pictus findings') + xlim(c(0,300))
 
-pdf('stats_lacatria.pdf')
+pdf('stats_db.pdf')
 grid.arrange(g1, g2, g3, ncol=1)
 dev.off()
 
