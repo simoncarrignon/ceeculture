6d5626d42b6e27327726835c9b8af8697fede6f0
Simon <simon.carrignon@gmail.com>
2016-12-13 12:25:05 +0100

    change on analysis but not sure what

--- AnalyseTools/analysis.R
+++ AnalyseTools/analysis.R
@@ -31,55 +31,6 @@ pointFitness=function(p1,b1){
     text(b1,-0.09,paste("n(g)=",b1,sep=""),srt=90,cex=1.2,pos=4,col="red")
 }
 
-###Thsi fuction read a csv and add the propertie associeted to a elelment find in the xml config file
-##Warning : do it just for "network" pro, should be extend to put a list of properties.
-readAddProp<-function(docname,...){
-    res=read.csv(paste(docname,"/agents.csv",sep=""),sep=";")
-
-    i=xmlTreeParse(paste(docname,"/config.xml",sep=""))
-    i=xmlRoot(i)
-    net=i[["network"]]
-    print(net)
-    for( u in 1:xmlSize(net)){
-	attri=xmlAttrs(net[[u]])
-	if(length(attri)>0){
-	    paramId=attri[["id"]]
-	    paramValue=as.numeric(attri[["value"]])
-	    newCol=rep(paramValue,nrow(res))
-	    res=cbind(res,newCol)
-	    names(res)[length(res)]=paramId
-	}
-
-    }
-    return(res)
-}
-
-
-###This return aa properties in an XML config file.
-##the XML file shoudl be on the form:
-## <type>
-##	<XXX id=propId value=valeureturned/>
-## </type>
-getPropFromXml <- function(expeDir,type,propId,format=as.numeric){
-    filename=paste(expeDir,"/config.xml",sep="")
-    i=xmlTreeParse(filename)
-    i=xmlRoot(i)
-    net=i[[type]]
-    if(length(net)<1){
-	print(paste("Object\'",type,"\' not defined in the XML file",filename))
-	return(NULL)
-    }
-    child=xmlChildren(net)
-    for(j in child){
-	a=xmlAttrs(j)
-	print(propId)
-	if(a["id"]==propId)
-	    return(format(a["value"]))
-    }
-    print(paste("Property \'", propId, "\' not defined in the XML file",filename))
-    return(NULL)
-}
-
 allCPhDlass<-function(datas,ngoods,g){
     nAgent=length(unique(datas$agent))
     a=sapply(c(0:nAgent)[0:(nAgent-1)%%(ngoods)==g],function(i){paste("Roman_",i,sep="")})
