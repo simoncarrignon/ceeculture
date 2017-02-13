8fc4ee32328804eda4fd6ab40cd84f670ac5e112
Simon Carrignon <sc@elisya.org>
2017-02-13 09:53:28 +0100

    simple script to run batch of expe

---
+++ runExp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for i in {1..200} ;
+do
+	mkdir exp_$i ; 
+	ln -s ../province exp_$i/; 
+	ln -s ../AnalyseTools/analysis exp_$i/; 
+	ln -s ../config.xml exp_$i; 
+	cd exp_$i ; 
+	./province ; 
+	./analysis ; 
+	cd .. ;
+done
