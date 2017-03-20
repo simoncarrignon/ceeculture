bb9194be272cfd6a7a1293f06e35a26d7d4a5809
Simon Carrignon <sc@elisya.org>
2017-03-20 15:22:36 +0100

    modif script runexp.sh to allow the creation of one folder for each experiment with the config file (the that helps to keep track of the parameters used)

--- runExp.sh
+++ runExp.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-for i in {1..200} ;
+name=$1
+mkdir $name
+cp config.xml $name
+cd $name
+for i in {92..200} ;
 do
 	mkdir exp_$i ; 
-	ln -s ../province exp_$i/; 
-	ln -s ../AnalyseTools/analysis exp_$i/; 
-	ln -s ../config.xml exp_$i; 
+	ln -s ../../province exp_$i/; 
+	ln -s ../../AnalyseTools/analysis exp_$i/; 
+	ln -s ../config.xml exp_$i/; 
 	cd exp_$i ; 
 	./province ; 
 	./analysis ; 
 	cd .. ;
 done
+cd ..
