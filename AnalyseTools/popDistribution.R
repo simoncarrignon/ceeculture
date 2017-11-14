f3e17205fa667b89527200b30506c126226a62a6
Simon Carrignon <simon.carrignon@gmail.com>
2017-11-14 17:15:16 +0100

    new fit to discret gama beta distribution

--- AnalyseTools/popDistribution.R
+++ AnalyseTools/popDistribution.R
@@ -9,6 +9,8 @@
 	countSize=countSize[countSize>0]/sum(countSize)
 	fit=lm(log2(countSize/sum(countSize)) ~ log2(as.numeric(names(countSize))))                                                     
 
+	fit=lm(log2(countSize/sum(countSize)) ~ log2(as.numeric(names(countSize))))                                                     
+
 	if(add)
 	    points(as.numeric(names(countSize)),as.numeric(countSize),...)
 	else
@@ -48,10 +50,16 @@ png("distribRank.png")
 dev.off() 
 png("distribRankFit.png")
 	fit=lm(log(sort(distrib))~log(order(distrib)))
+	fit=lm(log(sort(distrib))~log(order(distrib)))
 	fit2=lm(log(sort(distrib2))~log(order(distrib2)))
 	plot(sort(distrib) ~ order(distrib) ,xlab="rank",ylab="settlement size",log="yx")
-	points(exp(.001:7),exp(fit2$coefficients[1]+(.001:7)*fit2$coefficients[2]),col="green",type="l",lwd=3)
+	points(exp(.001:7),exp(fit$coefficients[1]+(.001:7)*fit$coefficients[2]),col="black",type="l",lwd=3)
+	points(exp(.001:7),exp(fit2$coefficients[1]+(.001:7)*fit2$coefficients[2]),col="red",type="l",lwd=3)
 	text(exp(7/2-3),exp((fit$coefficients[1]+(7/2)*fit$coefficients[2])),label=paste("y=ax+b :\n",round(fit$coefficients[2],2),"x+",round(fit$coefficients[1],2),"\nalpha=1-1/a ~",round(alpha,2)),col="orange",adj=0,lwd=4,cex=1.2)
+	text(exp(7/2-3),exp((fit2$coefficients[1]+(7/2)*fit2$coefficients[2])),label=paste("y=ax+b :\n",round(fit2$coefficients[2],2),"x+",round(fit2$coefficients[1],2),"\nalpha=1-1/a ~",round(alpha2,2)),col="black",adj=0,lwd=4,cex=1.2)
+
+		alpha = -1 + 1/fit$coefficients[2]     # It has to be negative.
+		alpha2 = -1 + 1/fit2$coefficients[2]     # It has to be negative.
 dev.off()
 
 png("distribRankSimAll.png")
@@ -120,5 +128,35 @@ softenedData <- function(dat){
 
 }
 
-plotDist <- function(dat) plot(as.numeric(table(signif(dat,2))) ~ as.numeric(names(table(signif(dat,2)))),log="xy")
+plotDist <- function(dat) plot(as.numeric(table(signif(dat,3))) ~ as.numeric(names(table(signif(dat,2)))),log="xy")
 
+fitNew <- function(dat){
+    dat=distrib
+    N=length(dat)
+    r =1:length(dat)
+    ex <- function(x,u,a,b)(u*(length(x)+1-x)^b)/(x^a)
+    points(r,ex(r,85199,.69,.21),log="yx",type="l",col="blue")
+        x=seq(0.01,7,length.out=length(r))
+    plot(dat~r, log = "xy",xlab = "Rank", ylab = "Population")
+    points(r,ex(r,exp(co[[1]]),-1*co[[3]],co[[2]]),col="green")
+    points(r,ex(r,coef(aa)[1],-1/coef(aa)[3],coef(aa)[2]),col="blue")
+    points(r,ex(r,coef(aa)[1],coef(aa)[2],coef(aa)[3]),col="blue")
+    points(r,ex(r,50000,.95,.54),col="blue",log="y")
+    lines(r,ex(r,exp(co[[1]]),-1*co[[3]],co[[2]]),col="green")
+
+    ##plot the fitting using the parameter determined using lm
+    points(exp(x),exp(fit$coefficients[1]+(x)*fit$coefficients[2]),col="orange")
+    lines(exp(x),exp(fit$coefficients[1]+(x)*fit$coefficients[2]),col="red")
+    log.f = log(dat)
+    log.inverse.rank = log(length(r)+1-r)
+    co=coef(lm(log(dat)~log.inverse.rank + log(r)))##fiting using a linear model
+    zmf = function(x) exp(co[[1]]+ co[[2]]*log(length(x)+1-x) + co[[3]]*log(x))
+    xx=c(1:length(Rank))
+    ###plot the fiting of the linear model
+    lines(zmf(xx)~xx, col = "red")
+    points(zmf(xx)~xx, col = "purple")
+    #####my fitting using nls
+    fit3=coef(nls(dat ~ ex(r,u,a,b),lower=list(u=00,a=0,b=0.28),start=list(u=70000,a=0.7,b=0.5),algorithm="port"))
+    ##plot the fitting using the parameter determined using nls
+    points(r,ex(r,fit3[["u"]],fit3[["a"]],fit3[["b"]]),col="blue")
+}
